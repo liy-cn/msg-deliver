5a2b7b97d90470d8e17c0df15db56c48e7976ac3
liyue130 <liyue20@jd.com>
2021-03-06 22:32:59 +0800

    初次提交

---
+++ src/main/java/cn/liy/messagedeliver/MessageDeliverApplication.java
@@ -0,0 +1,13 @@
+package cn.liy.messagedeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageDeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageDeliverApplication.class, args);
+    }
+
+}
